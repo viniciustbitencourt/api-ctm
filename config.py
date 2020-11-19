3fdfcd5cb3f1f508ca1989461f8cc786cffc4200
viniciustbitencourt <vn.biten@gmail.com>
2020-11-19 12:31:11 -0300

    refatorar codigo

---
+++ config.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python2.7
+
+import base64
+
+usuario_ctm = 'user_teste'
+senha_ctm = base64.b64decode(b'ZW1wYXNz').decode('utf-8')
--- rest-api-ctm.py
+++ rest-api-ctm.py
@@ -5,13 +5,19 @@ import requests
 import urllib3
 import base64
 
+from config import usuario_ctm as usuario
+from config import senha_ctm as senha
+
 parser = argparse.ArgumentParser(description='Parse Argument')
 
 parser.add_argument('endpoint', type=str, help='Extensao do arquivo')
-parser.add_argument('porta', type=int, help='Porta da api em rest Swagger - Exemplo: 8443')
+parser.add_argument('port', type=int, help='Porta da api em rest Swagger - Exemplo: 8443')
 parser.add_argument('url', type=str, help='Url para a request exemplo: /session/login')
 parser.add_argument('method', type=str, help='Metodo POST')
 parser.add_argument('jobname', type=str, help='Colocar o jobname - Exemplo: PDDL0232')
+#parser.add_argument('application', type=str, default=None, help='Para pesquisar um status do job, Exemplo EndedOK or EndedNotOK')
+#parser.add_argument('status', type=str, default=None, help='Para pesquisar um status do job, Exemplo EndedOK or EndedNotOK')
+
 args = parser.parse_args()
 
 urllib3.disable_warnings()
@@ -20,41 +26,44 @@ urllib3.disable_warnings()
 #----
 
 def executa_job(args):
-    endPoint = 'https://Endpoint-URL:8443/automation-api'
-    user = 'your_username'
-    passwd = 'your_password'
+    
+    endPoint = 'https://{endpoint}:{port}/automation-api'.format(endpoint=args.endpoint, port=args.port)
+    user = '{usuario}'.format(usuario=usuario)
+    passwd = '{senha}'.format(senha=senha)
     mensagem = ''
+
     try:
         r_login = requests.post(endPoint + '/session/login', json={"username": user, "password": passwd}, verify=False)
-        print(r_login.content)
-        print(r_login.status_code)
+        #print(r_login.content)
+        #print(r_login.status_code)
 
         if r_login.status_code != requests.codes.ok:
-            exit(1)
+            exit(1) 
         token = r_login.json()['token']
 
         if args.method:
             if args.method == 'GET':
                 try:
-                    r = requests.get(endPoint + '/config/servers', headers={'Authorization': 'Bearer ' + token}, verify=False)
+                    r = requests.get(endPoint + '{url}?jobname={jobname}'.format(url=args.url, jobname=args.jobname), headers={'Authorization': 'Bearer ' + token}, verify=False)
                     print(r.content)
                     print(r.status_code)
                     exit(r.status_code == requests.codes.ok)
-
+                    r_logout = requests.post(endPoint + '/session/logout', json={"username": user, "password": passwd}, verify=False)
                 except requests.exceptions as e:
                     mensagem = 'Exception {e}'.format(e=e)
                     r = requests.get(args.url)
 
             if args.method == 'POST':
                 try:
-                    r = requests.post(endPoint + '/config/servers', headers={'Authorization': 'Bearer ' + token}, verify=False)
+                    r = requests.post(endPoint + '{url}'.format(url=args.url), headers={'Authorization': 'Bearer ' + token}, verify=False)
                     print(r.content)
                     print(r.status_code)
                     exit(r.status_code == requests.codes.ok)
+                    r_logout = requests.post(endPoint + '/session/logout', json={"username": user, "password": passwd}, verify=False)
                 except requests.exceptions as e:
                     mensagem = 'Exception {e}'.format(e=e)
                     r = requests.get(args.url)
-
+    
     except Exception as e:
         mensagem = 'erro'
 
